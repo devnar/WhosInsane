60994e77127c1262765ffae27a1d598d36c15ea1
NAR <yyazok@gmail.com>
2023-12-06 21:41:10 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -28,17 +28,6 @@ export default function Footer() {
                                             className="w-5 h-5 ml-2 -mr-1 transform rotate-180 text-violet-200 hover:text-violet-100"
                                             aria-hidden="true"
                                         />
-                                        <img
-                                            src={`https://flagcdn.com/w80/tr.png`}
-                                            width="24"
-                                            height="18"
-                                            className="mr-2 h-4 rounded-sm"
-                                        />
-                                        Türkçe
-                                        <ChevronDownIcon
-                                            className="w-5 h-5 ml-2 -mr-1 transform rotate-180 text-violet-200 hover:text-violet-100"
-                                            aria-hidden="true"
-                                        />
                                     </Menu.Button>
                                 </div>
                             </Menu>
